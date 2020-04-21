5df42693aff6e7de3270f2abfbb4830a8d42093e
Dilara Kayhan <53559729+dilarakayhan@users.noreply.github.com>
2020-04-21 12:55:05 +0200

    testground: print and variables

---
+++ testground.py
@@ -0,0 +1,9 @@
+numberThree = 3
+stringHello = "Hello"
+booleanTrue = True
+
+print(stringHello)
+print("This should be number 3: ", numberThree)
+print("True or false: Is the above number number 3? ", booleanTrue)
+
+print("All of them combined: ", stringHello, numberThree, booleanTrue)
